07d239e4c75585d7879aed3250bc43677cd4101f
(JAKSON) GuilhermeEnnes <jaksonguilherme2@gmail.com>
2023-05-24 23:54:31 -0300

    Mudança para que seja possível controlar o motor pelo analógico.

--- main.ino
+++ main.ino
@@ -9,7 +9,7 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 #define VERT_PIN A0
 #define HORZ_PIN A1
-#define SEL_PIN  2
+#define SEL_PIN  A2
 
 #define Ap 3  // A+ line
 #define An 2  // A- line
@@ -21,11 +21,11 @@ void setup() {
   pinMode(VERT_PIN, INPUT);
   pinMode(HORZ_PIN, INPUT);
   pinMode(SEL_PIN, INPUT_PULLUP);
-
-  pinMode(Ap, OUTPUT)
-  pinMode(An, OUTPUT)
-  pinMode(Bp, OUTPUT)
-  pinMode(Bn, OUTPUT)
+  
+  pinMode(Ap, OUTPUT);
+  pinMode(An, OUTPUT);
+  pinMode(Bp, OUTPUT);
+  pinMode(Bn, OUTPUT);
 
   lcd.init();
   lcd.backlight();
@@ -39,9 +39,49 @@ void loop() {
   int horz = analogRead(HORZ_PIN);
   bool selPressed = digitalRead(SEL_PIN) == LOW;
 
+  vert = map(vert, 0, 1023, -100, 100);
+  horz = map(horz, 0, 1023, 100, -100);
+  analogWrite(HORZ_PIN,horz);
+  analogWrite(VERT_PIN,vert);
+
+  
+
+  if (vert > 0 || horz > 0) {
+    digitalWrite(Ap,LOW) ;  digitalWrite(Bn,HIGH);
+    delay(5);
+    
+    digitalWrite(Bn,LOW) ;  digitalWrite(An,HIGH);
+    delay(5);
+    
+    digitalWrite(An,LOW) ;  digitalWrite(Bp,HIGH);
+    delay(5);
+    
+    digitalWrite(Bp,LOW) ;  digitalWrite(Ap,HIGH);
+    delay(5);
+  } else if (horz < 0 || vert < 0) {
+    digitalWrite(Ap,LOW) ;  digitalWrite(Bp,HIGH);
+    delay(5);
+    digitalWrite(Bp,LOW) ;  digitalWrite(An,HIGH);
+    delay(5);
+    digitalWrite(An,LOW) ;  digitalWrite(Bn,HIGH);
+    delay(5);
+    digitalWrite(Bn,LOW) ;  digitalWrite(Ap,HIGH);
+    delay(5);
+  }
+
+
+
   lcd.setCursor(7, 1);
   lcd.print(vert);
 
   lcd.setCursor(7, 2);
   lcd.print(horz);
+  lcd.setCursor(8, 3);
+  lcd.print(selPressed);
+
+
+  if (digitalRead(SEL_PIN) == LOW){
+    lcd.setCursor(7,2);
+    lcd.print("pudim");
+  }
 }
